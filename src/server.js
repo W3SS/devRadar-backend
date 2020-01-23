e57952aa076991adb261667598d7b09e04b7e534
Wendel G. Santana <gswendelw3ss@gmail.com>
2020-01-23 11:24:47 -0300

    Correção no modulo de rotas.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const expressPino = require('express-pino-logger')
 const expressMetrics = require('express-metrics')
 const mongoose = require('mongoose')
 const compression = require('compression')
@@ -15,27 +14,18 @@ require('dotenv').config({ encoding: 'utf8' })
 
 const app = express()
 
-// mongoose.connect(
-//   process.env.MONGO_CONNECTION,{
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
+mongoose.connect(
+  process.env.MONGO_CONNECTION,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
-app.use(cors())
 app.use(express.json())
 app.use(expressMetrics({
   port: 8091
 }));
 
-const expressLogger = expressPino({ logger })
-app.use(expressLogger)
-app.use((req, res, next) => {
-  logger.trace(
-    { req, requestId: req.headers['X-Request-Id'] },
-    'A new request received at ' + Date.now()
-  )
-  next()
-})
+app.use(cors())
 
 app.use(helmet())
 app.use(compression())
@@ -44,20 +34,22 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cookieParser())
+
 app.use(routes)
 
-app.use(function(err, req, res, next) {
-  logger.error(err)
+
+app.use((req, res, next) => {
   res.status(500)
   res.send('Oops, algo inesperado ocorreu!')
 })
 
 /**
- * Listen do expressJS
+ * Listening Port 
  */
+
 if (require.main === module) {
   app.listen(process.env.PORT, (req, res) => {
-    logger.info('Listening on port', process.env.PORT)
+    console.log('Server listening on port', process.env.PORT)
   })
 } else {
   module.exports = { app }
